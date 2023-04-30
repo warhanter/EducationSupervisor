7a104a6b396e2c07c5aba288ea8386c635406240
warhanter <pakeryoko@gmail.com>
2023-04-30 21:54:14 +0100

    configuare vite base config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // ** build for Github Pages //
   base: "/EducationSupervisor/",
+
+  // ** build for local testing //
+  // base: "/",
 });
