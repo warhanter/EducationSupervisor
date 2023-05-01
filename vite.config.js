7ecd0102f72be21c730c09c6cd556339d7807c8e
warhanter <pakeryoko@gmail.com>
2023-05-01 01:47:41 +0100

    publicDir: "/EducationSupervisor/public",

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/EducationSupervisor/",
-  publicDir: "public/EducationSupervisor/",
+  publicDir: "/EducationSupervisor/public",
 });
