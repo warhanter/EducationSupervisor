d20d6fb5b558ce6c1c7e3a23392ff3587feb5c5a
warhanter <pakeryoko@gmail.com>
2023-05-06 00:49:05 +0100

    aa6

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/EducationSupervisor/",
+  base: "https://warhanter.github.io/EducationSupervisor/",
   // base: "/",
 });
