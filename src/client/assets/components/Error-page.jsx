57cdd0d1c3fe6497417fa4d1398806fc010b7e30
warhanter <pakeryoko@gmail.com>
2023-05-05 17:38:12 +0100

    wrap error message win Card component

--- src/client/assets/components/Error-page.jsx
+++ src/client/assets/components/Error-page.jsx
@@ -1,4 +1,4 @@
-import { Container } from "react-bootstrap";
+import { Card, Container } from "react-bootstrap";
 import { NavLink, useRouteError } from "react-router-dom";
 
 export default function ErrorPage() {
@@ -11,14 +11,18 @@ export default function ErrorPage() {
       style={{ minHeight: "100vh" }}
     >
       <div className="w-100" style={{ maxWidth: 400 }}>
-        <h1>Oops!</h1>
-        <p>Sorry, an unexpected error has occurred.</p>
-        <p style={{ color: "orangered" }}>
-          <i>{error.statusText || error.message}</i>
-        </p>
-        <NavLink style={{ alignSelf: "center" }} to={"/"}>
-          Back to Home
-        </NavLink>
+        <Card>
+          <h1 className="m-2">404 Not Found!</h1>
+          <Card.Body>
+            <p>Sorry, an unexpected error has occurred.</p>
+            <p style={{ color: "orangered" }}>
+              <i>{error.statusText || error.message}</i>
+            </p>
+            <NavLink style={{ alignSelf: "center" }} to={"/"}>
+              Back to Home
+            </NavLink>
+          </Card.Body>
+        </Card>
       </div>
     </Container>
   );
