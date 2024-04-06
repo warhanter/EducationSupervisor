c8f609f164f11b87d46d245913aa4ed0a24a1168
warhanter <pakeryoko@gmail.com>
2024-04-06 04:35:50 +0100

    fix: Duplicate key base in object literal

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
     }),
   ],
   base: "/EducationSupervisor/",
-  base: "/",
+  // base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
