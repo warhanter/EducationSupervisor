abe7d15b562f33e614f3721495fe8a256c75950e
warhanter <pakeryoko@gmail.com>
2024-04-07 01:19:44 +0100

    chore: ui changes.

--- src/client/assets/components/HeaderNavbar.tsx
+++ src/client/assets/components/HeaderNavbar.tsx
@@ -35,11 +35,11 @@ function HeaderNavbar() {
     <header className="sticky z-50 top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
       <nav className="hidden whitespace-nowrap flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
         <Link
-          to="#"
+          to="/"
           className="flex items-center gap-2 text-lg font-semibold md:text-base"
         >
           <School className="h-6 w-6" />
-          <span className="sr-only">Acme Inc</span>
+          <span className="sr-only">مساعد المشرف</span>
         </Link>
         <AppLink to="/" link="الرئيسية" />
         <AppLink to="/allStudents" link="كل التلاميذ" />
