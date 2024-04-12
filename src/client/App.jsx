250acc9927ea0236261b743f43bcdcaf97ef5ec9
warhanter <pakeryoko@gmail.com>
2024-04-12 14:33:44 +0100

    chore:: browserRouter..

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -70,8 +70,7 @@ function App() {
         </Route>
         <Route path="/login" element={<LoginForm />} />
       </>
-    ),
-    { basename: "/EducationSupervisor/" }
+    )
   );
   return (
     <AuthProvider>
