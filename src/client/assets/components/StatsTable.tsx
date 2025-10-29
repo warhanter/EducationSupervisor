fec75dfbafeadc1cee0aa4cb49c3b415bac4e2bc
Walid Saidi <pakeryoko@gmail.com>
2025-10-29 23:15:04 +0100

    feature: add stats table to the print page

--- src/client/assets/components/StatsTable.tsx
+++ src/client/assets/components/StatsTable.tsx
@@ -10,6 +10,46 @@ type MaafiyinPrintTableProps = PDFPrintTablesProps & {
   date: number;
 };
 
+const calcLength = (
+  students: Record<string, any>[],
+  student_status: string,
+  mamnouh?: boolean
+) => {
+  if (mamnouh !== undefined) {
+    const len = students?.filter(
+      (s: any) =>
+        s.student_status === student_status && s.is_mamnouh === mamnouh
+    ).length;
+    return len;
+  }
+  const len = students?.filter(
+    (s: any) => s.student_status === student_status
+  ).length;
+  return len;
+};
+
+const calcStats = (students: any[]) => {
+  const males = students?.filter((s) => s.gender === "ذكر");
+  const females = students?.filter((s) => s.gender === "أنثى");
+  const stats: Record<string, number> = {};
+
+  stats.dakhiliMales = calcLength(males, "داخلي");
+  stats.dakhiliFemales = calcLength(females, "داخلي");
+
+  stats.nisfDakhiliMales = calcLength(males, "نصف داخلي");
+  stats.nisfDakhiliFemales = calcLength(females, "نصف داخلي");
+
+  stats.mamnouhinMales = calcLength(males, "نصف داخلي", true);
+  stats.mamnouhinFemales = calcLength(females, "نصف داخلي", true);
+
+  stats.mosadidinMales = calcLength(males, "نصف داخلي", false);
+  stats.mosadidinFemales = calcLength(females, "نصف داخلي", false);
+
+  stats.kharijiMales = calcLength(males, "خارجي");
+  stats.kharijiFemales = calcLength(females, "خارجي");
+  return stats;
+};
+
 // Reusable header component
 const DocumentHeader = () => (
   <>
@@ -43,117 +83,166 @@ const DocumentFooter = ({ date }: { date: string }) => {
   );
 };
 
+const TableRowData = ({ text, colSpan = 1, rowSpan = 1, type = "data" }) => {
+  if (type === "head") {
+    return (
+      <th
+        colSpan={colSpan}
+        rowSpan={rowSpan}
+        className="border-separate border border-zinc-500 py-1 px-1 bg-gray-200"
+      >
+        {text}
+      </th>
+    );
+  }
+  return (
+    <td
+      colSpan={colSpan}
+      rowSpan={rowSpan}
+      className="text-center border-separate border border-zinc-500 py-1 px-1"
+    >
+      {text}
+    </td>
+  );
+};
+
 // Table header component
 const StatsTableHeader = ({ dakhili }: { dakhili: number }) => {
   return (
     <thead className="text-center border-separate border border-zinc-500 bg-gray-200">
       <tr>
-        <th
-          rowSpan={2}
-          className="border-separate border border-zinc-500 py-1 px-1 bg-gray-200"
-        >
-          القسم
-        </th>
+        <TableRowData type="head" rowSpan={2} text="القسم" />
         {/* {dakhili && (
-          <th
+          <TableRowData type="head"
             colSpan={3}
-            className="text-center border-separate border border-zinc-500 py-1 px-1 bg-gray-200 "
-          >
-            داخلي
-          </th>
+            text="داخلي"
+          />
         )} */}
-        <th
-          colSpan={3}
-          className="text-center border-separate border border-zinc-500 py-1 px-1 bg-gray-200"
-        >
-          نصف داخلي
-        </th>
-        <th
-          colSpan={3}
-          className="text-center border-separate border border-zinc-500 py-1 px-1 bg-gray-200"
-        >
-          ممنوحين
-        </th>
-        <th
-          colSpan={3}
-          className="text-center border-separate border border-zinc-500 py-1 px-1 bg-gray-200"
-        >
-          مسددين
-        </th>
-        <th
-          colSpan={3}
-          className="text-center border-separate border border-zinc-500 py-1 px-1 bg-gray-200"
-        >
-          خارجي
-        </th>
-        <th
-          colSpan={3}
-          className="text-center border-separate border border-zinc-500 py-1 px-1 bg-gray-200"
-        >
-          الكل
-        </th>
+        <TableRowData type="head" colSpan={3} text="نصف داخلي" />
+        <TableRowData type="head" colSpan={3} text="ممنوحين" />
+        <TableRowData type="head" colSpan={3} text="مسددين" />
+        <TableRowData type="head" colSpan={3} text="خارجي" />
+        <TableRowData type="head" colSpan={3} text="الكل" />
       </tr>
       <tr>
         {/* {dakhili && (
           <>
-            <th className="text-center border-separate border border-zinc-500 py-1 px-1 bg-gray-200">
-              ذ
-            </th>
-            <th className="text-center border-separate border border-zinc-500 py-1 px-1 bg-gray-200">
-              إ
-            </th>
-            <th className="text-center border-separate border border-zinc-500 py-1 px-1 bg-gray-200">
-              م
-            </th>
+            <TableRowData type="head" text="ذ"
+              
+            />
+            <TableRowData type="head" text=""
+              
+            />
+            <TableRowData type="head" text="م"
+              
+            />
           </>
         )} */}
-        <th className="text-center border-separate border border-zinc-500 py-1 px-1 bg-gray-200">
-          ذ
-        </th>
-        <th className="text-center border-separate border border-zinc-500 py-1 px-1 bg-gray-200">
-          إ
-        </th>
-        <th className="text-center border-separate border border-zinc-500 py-1 px-1 bg-gray-200">
-          م
-        </th>
-        <th className="text-center border-separate border border-zinc-500 py-1 px-1 bg-gray-200">
-          ذ
-        </th>
-        <th className="text-center border-separate border border-zinc-500 py-1 px-1 bg-gray-200">
-          إ
-        </th>
-        <th className="text-center border-separate border border-zinc-500 py-1 px-1 bg-gray-200">
-          م
-        </th>
-        <th className="text-center border-separate border border-zinc-500 py-1 px-1 bg-gray-200">
-          ذ
-        </th>
-        <th className="text-center border-separate border border-zinc-500 py-1 px-1 bg-gray-200">
-          إ
-        </th>
-        <th className="text-center border-separate border border-zinc-500 py-1 px-1 bg-gray-200">
-          م
-        </th>
-        <th className="text-center border-separate border border-zinc-500 py-1 px-1 bg-gray-200">
-          ذ
-        </th>
-        <th className="text-center border-separate border border-zinc-500 py-1 px-1 bg-gray-200">
-          إ
-        </th>
-        <th className="text-center border-separate border border-zinc-500 py-1 px-1 bg-gray-200">
-          م
-        </th>
-        <th className="text-center border-separate border border-zinc-500 py-1 px-1 bg-gray-200">
-          ذ
-        </th>
-        <th className="text-center border-separate border border-zinc-500 py-1 px-1 bg-gray-200">
-          إ
-        </th>
-        <th className="text-center border-separate border border-zinc-500 py-1 px-1 bg-gray-200">
-          م
-        </th>
+        <TableRowData type="head" text="ذ" />
+        <TableRowData type="head" text="إ" />
+        <TableRowData type="head" text="م" />
+        <TableRowData type="head" text="ذ" />
+        <TableRowData type="head" text="إ" />
+        <TableRowData type="head" text="م" />
+        <TableRowData type="head" text="ذ" />
+        <TableRowData type="head" text="إ" />
+        <TableRowData type="head" text="م" />
+        <TableRowData type="head" text="ذ" />
+        <TableRowData type="head" text="إ" />
+        <TableRowData type="head" text="م" />
+        <TableRowData type="head" text="ذ" />
+        <TableRowData type="head" text="إ" />
+        <TableRowData type="head" text="م" />
       </tr>
     </thead>
+  );
+};
+
+// Table header component
+const StatsTableFooter = ({
+  students,
+  dakhili,
+}: {
+  students: Record<string, any>[];
+  dakhili: number;
+}) => {
+  const stats = calcStats(students);
+
+  return (
+    <tfoot className="text-center border-separate border border-zinc-500 bg-gray-200">
+      <tr key={"الجمــــــــوع"}>
+        <td className="border border-collapse border-zinc-500 py-1 px-1">
+          {"الجمــــــــوع"}
+        </td>
+        {dakhili >= 1 && (
+          <>
+            <td className="text-center border border-collapse border-zinc-500 py-1 px-1">
+              {stats.dakhiliMales}
+            </td>
+            <td className="text-center border border-collapse border-zinc-500 py-1 px-1">
+              {stats.dakhiliFemales}
+            </td>
+            <td className="text-center border border-collapse border-zinc-500 py-1 px-1 bg-gray-200">
+              {stats.dakhiliMales + stats.dakhiliFemales}
+            </td>
+          </>
+        )}
+        {/* //nisfDakhili */}
+        <TableRowData type="head" text={stats.nisfDakhiliMales} />
+        <TableRowData type="head" text={stats.nisfDakhiliFemales} />
+        <TableRowData
+          type="head"
+          text={stats.nisfDakhiliMales + stats.nisfDakhiliFemales}
+        />
+        {/* //mamnouhin */}
+        <TableRowData type="head" text={stats.mamnouhinMales} />
+        <TableRowData type="head" text={stats.mamnouhinFemales} />
+        <TableRowData
+          type="head"
+          text={stats.mamnouhinMales + stats.mamnouhinFemales}
+        />
+        {/* //mosadidin */}
+        <TableRowData type="head" text={stats.mosadidinMales} />
+        <TableRowData type="head" text={stats.mosadidinFemales} />
+        <TableRowData
+          type="head"
+          text={stats.mosadidinMales + stats.mosadidinFemales}
+        />
+        {/* //khariji */}
+        <TableRowData type="head" text={stats.kharijiMales} />
+        <TableRowData type="head" text={stats.kharijiFemales} />
+        <TableRowData
+          type="head"
+          text={stats.kharijiMales + stats.kharijiFemales}
+        />
+        {/* // total */}
+        <TableRowData
+          type="head"
+          text={
+            stats.dakhiliMales + stats.nisfDakhiliMales + stats.kharijiMales
+          }
+        />
+        <TableRowData
+          type="head"
+          text={
+            stats.dakhiliFemales +
+            stats.nisfDakhiliFemales +
+            stats.kharijiFemales
+          }
+        />
+        <TableRowData
+          type="head"
+          text={
+            stats.dakhiliMales +
+            stats.nisfDakhiliMales +
+            stats.kharijiMales +
+            stats.dakhiliFemales +
+            stats.nisfDakhiliFemales +
+            stats.kharijiFemales
+          }
+        />
+      </tr>
+    </tfoot>
   );
 };
 
@@ -162,19 +251,18 @@ const StatsTableRow = ({
   key,
   stats,
   className,
+  type,
 }: {
   key: string;
   stats: any;
   className: string;
-  dakhili: number;
+  type?: string;
 }) => {
   const dakhili = stats.dakhiliMales + stats.dakhiliFemales;
   return (
     <tr key={key}>
-      <td className="border border-collapse border-zinc-500 py-1 px-1">
-        {className}
-      </td>
-      {/* {dakhili && (
+      <TableRowData type={type} text={className} />
+      {dakhili >= 1 && (
         <>
           <td className="text-center border border-collapse border-zinc-500 py-1 px-1">
             {stats.dakhiliMales}
@@ -186,65 +274,65 @@ const StatsTableRow = ({
             {stats.dakhiliMales + stats.dakhiliFemales}
           </td>
         </>
-      )} */}
+      )}
       {/* //nisfDakhili */}
-      <td className="text-center border border-collapse border-zinc-500 py-1 px-1">
-        {stats.nisfDakhiliMales}
-      </td>
-      <td className="text-center border border-collapse border-zinc-500 py-1 px-1">
-        {stats.nisfDakhiliFemales}
-      </td>
-      <td className="text-center border border-collapse border-zinc-500 py-1 px-1 bg-gray-200">
-        {stats.nisfDakhiliMales + stats.nisfDakhiliFemales}
-      </td>
+      <TableRowData type={type} text={stats.nisfDakhiliMales} />
+      <TableRowData type={type} text={stats.nisfDakhiliFemales} />
+      <TableRowData
+        type="head"
+        text={stats.nisfDakhiliMales + stats.nisfDakhiliFemales}
+      />
       {/* //mamnouhin */}
-      <td className="text-center border border-collapse border-zinc-500 py-1 px-1">
-        {stats.mamnouhinMales}
-      </td>
-      <td className="text-center border border-collapse border-zinc-500 py-1 px-1">
-        {stats.mamnouhinFemales}
-      </td>
-      <td className="text-center border border-collapse border-zinc-500 py-1 px-1 bg-gray-200">
-        {stats.mamnouhinMales + stats.mamnouhinFemales}
-      </td>
+      <TableRowData type={type} text={stats.mamnouhinMales} />
+      <TableRowData type={type} text={stats.mamnouhinFemales} />
+      <TableRowData
+        type="head"
+        text={stats.mamnouhinMales + stats.mamnouhinFemales}
+      />
       {/* //mosadidin */}
-      <td className="text-center border border-collapse border-zinc-500 py-1 px-1">
-        {stats.mosadidinMales}
-      </td>
-      <td className="text-center border border-collapse border-zinc-500 py-1 px-1">
-        {stats.mosadidinFemales}
-      </td>
-      <td className="text-center border border-collapse border-zinc-500 py-1 px-1 bg-gray-200">
-        {stats.mosadidinMales + stats.mosadidinFemales}
-      </td>
+      <TableRowData type={type} text={stats.mosadidinMales} />
+      <TableRowData type={type} text={stats.mosadidinFemales} />
+      <TableRowData
+        type="head"
+        text={stats.mosadidinMales + stats.mosadidinFemales}
+      />
       {/* //khariji */}
-      <td className="text-center border border-collapse border-zinc-500 py-1 px-1">
-        {stats.kharijiMales}
-      </td>
-      <td className="text-center border border-collapse border-zinc-500 py-1 px-1">
-        {stats.kharijiFemales}
-      </td>
-      <td className="text-center border border-collapse border-zinc-500 py-1 px-1 bg-gray-200">
-        {stats.kharijiMales + stats.kharijiFemales}
-      </td>
+      <TableRowData type={type} text={stats.kharijiMales} />
+      <TableRowData type={type} text={stats.kharijiFemales} />
+      <TableRowData
+        type="head"
+        text={stats.kharijiMales + stats.kharijiFemales}
+      />
       {/* // total */}
-      <td className="text-center border border-collapse border-zinc-500 py-1 px-1">
-        {stats.dakhiliMales + stats.nisfDakhiliMales + stats.kharijiMales}
-      </td>
-      <td className="text-center border border-collapse border-zinc-500 py-1 px-1">
-        {stats.dakhiliFemales + stats.nisfDakhiliFemales + stats.kharijiFemales}
-      </td>
-      <td className="text-center border border-collapse border-zinc-500 py-1 px-1 bg-gray-200">
-        {stats.dakhiliMales +
+      <TableRowData
+        type={type}
+        text={stats.dakhiliMales + stats.nisfDakhiliMales + stats.kharijiMales}
+      />
+      <TableRowData
+        type={type}
+        text={
+          stats.dakhiliFemales + stats.nisfDakhiliFemales + stats.kharijiFemales
+        }
+      />
+      <TableRowData
+        type="head"
+        text={
+          stats.dakhiliMales +
           stats.nisfDakhiliMales +
           stats.kharijiMales +
           stats.dakhiliFemales +
           stats.nisfDakhiliFemales +
-          stats.kharijiFemales}
-      </td>
+          stats.kharijiFemales
+        }
+      />
     </tr>
   );
 };
+
+// const StatsTableFooter = ({students}) => {
+//   const
+
+// }
 
 // Single class table component
 const ClassTable = ({
@@ -254,46 +342,32 @@ const ClassTable = ({
   students: any[];
   className: string;
 }) => {
-  const males = students?.filter((s) => s.gender === "ذكر");
-  const females = students?.filter((s) => s.gender === "أنثى");
+  const studentsGroupedByClass = groupBy(students, "full_class_name");
+  const stats = calcStats(students);
 
-  const stats: Record<string, number> = {};
-
-  stats.dakhiliMales = males?.filter(
-    (s) => s.student_status === "داخلي"
-  ).length;
-  stats.dakhiliFemales = females?.filter(
-    (s) => s.student_status === "داخلي"
-  ).length;
-
-  stats.nisfDakhiliMales = males?.filter(
-    (s) => s.student_status === "نصف داخلي"
-  ).length;
-  stats.nisfDakhiliFemales = females?.filter(
-    (s) => s.student_status === "نصف داخلي"
-  ).length;
-
-  stats.mamnouhinMales = males?.filter(
-    (s) => s.student_status === "نصف داخلي" && s.is_mamnouh
-  ).length;
-  stats.mamnouhinFemales = females?.filter(
-    (s) => s.student_status === "نصف داخلي" && s.is_mamnouh
-  ).length;
-
-  stats.mosadidinMales = males?.filter(
-    (s) => s.student_status === "نصف داخلي" && !s.is_mamnouh
-  ).length;
-  stats.mosadidinFemales = females?.filter(
-    (s) => s.student_status === "نصف داخلي" && !s.is_mamnouh
-  ).length;
-
-  stats.kharijiMales = males?.filter(
-    (s) => s.student_status === "خارجي"
-  ).length;
-  stats.kharijiFemales = females?.filter(
-    (s) => s.student_status === "خارجي"
-  ).length;
-  return <StatsTableRow key={className} stats={stats} className={className} />;
+  // return <StatsTableRow key={className} stats={stats} className={className} />;
+  return (
+    <>
+      {Object.entries(studentsGroupedByClass).map(([class_name, students]) => {
+        const stats = calcStats(students);
+        return (
+          <StatsTableRow
+            key={class_name}
+            stats={stats}
+            className={class_name}
+          />
+        );
+      })}
+      {Object.entries(studentsGroupedByClass).length > 1 && (
+        <StatsTableRow
+          key={className}
+          stats={stats}
+          className={className}
+          type="head"
+        />
+      )}
+    </>
+  );
 };
 
 // Main component
@@ -302,7 +376,11 @@ export default function StatsTable({
   date,
   title,
 }: MaafiyinPrintTableProps) {
-  const studentsGroupedByClass = groupBy(data, "full_class_name");
+  const studentsGroupedByClassName = groupBy(
+    data,
+    (s) => `${s.level}_${s.class_name}`
+  );
+
   const dakhili = data?.filter((s) => s.student_status === "داخلي").length;
 
   return (
@@ -312,7 +390,7 @@ export default function StatsTable({
       <table className="w-full print:text-[14px] font-medium">
         <StatsTableHeader dakhili={dakhili ?? 0} />
         <tbody>
-          {Object.entries(studentsGroupedByClass).map(
+          {Object.entries(studentsGroupedByClassName).map(
             ([className, students]) => (
               <ClassTable
                 key={className}
@@ -324,6 +402,7 @@ export default function StatsTable({
             )
           )}
         </tbody>
+        <StatsTableFooter students={data} dakhili={dakhili} />
       </table>
       <DocumentFooter date={date} />
     </div>
