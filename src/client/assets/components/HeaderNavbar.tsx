945d054e878c2d1182d9770e5c8c551e00558735
Walid Saidi <pakeryoko@gmail.com>
2025-11-18 01:16:36 +0100

    ui changes: remove the search bar on the navBar

--- src/client/assets/components/HeaderNavbar.tsx
+++ src/client/assets/components/HeaderNavbar.tsx
@@ -39,73 +39,79 @@ function HeaderNavbar() {
     setLoading(false);
   };
   return (
-    <header className="sticky z-50 top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
-      <nav className="hidden whitespace-nowrap flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
-        <Link
-          to="/"
-          className="flex items-center gap-2 text-lg font-semibold md:text-base"
-        >
-          <School className="h-6 w-6" />
-          <span className="sr-only">مساعد المشرف</span>
-        </Link>
-        <AppLink to="/" link="الرئيسية" />
-        <AppLink to="/allStudents" link="كل التلاميذ" />
-        <AppLink to="/absences" link="الغيابات" />
-        <AppLink to="/mark-absences" link="تسجيل الغيابات" />
-        {/* <AppLink to="/absences" link="اشعارات" /> */}
-        <NavigationMenu className="text-right">
-          <NavigationMenuList>
-            <NavigationMenuItem>
-              <NavigationMenuTrigger className="text-muted-foreground transition-colors hover:text-foreground">
-                قوائم
-              </NavigationMenuTrigger>
-              <NavigationMenuContent>
-                <ul className="grid gap-1 p-4">
-                  <AppLink to="/maafiyin" link="المعفيين" subLink />
-                  <AppLink to="/moghadirin" link="المغادرين" subLink />
-                  <AppLink to="/wafidin" link="الوافدين" subLink />
-                  <AppLink to="/otlaMaradiya" link="العطل المرضية" subLink />
-                  <AppLink to="/mo3idin" link="المعيدين" subLink />
-                  <AppLink to="/mamnouhin" link="الممنوحين" subLink />
-                  <AppLink to="/mosadidin" link="المسددين" subLink />
-                </ul>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
-          </NavigationMenuList>
-        </NavigationMenu>
-        <AppLink to="/print-pdf" link="طباعة" />
-        <AppLink to="/student-cards" link="بطاقات" />
-        <AppLink to="/add-student" link="إضافة تلميذ" />
-        {/* <AppLink to="/daily-report" link="التقرير اليومي" /> */}
-      </nav>
-      <Sheet>
-        <SheetTrigger asChild>
-          <Button variant="outline" size="icon" className="shrink-0 md:hidden">
-            <Menu className="h-5 w-5" />
-            <span className="sr-only">Toggle navigation menu</span>
-          </Button>
-        </SheetTrigger>
-        <SheetContent side="right" id="sheet">
-          <nav dir="rtl" className="grid gap-6 text-lg font-medium">
-            <Link
-              to="/"
-              className="flex items-center gap-2 text-lg font-semibold"
+    <header className="sticky z-50 top-0 flex h-16 justify-between w-full items-center  border-b bg-background px-4 md:px-6">
+      <div className="flex items-center">
+        <nav className="hidden whitespace-nowrap flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+          <Link
+            to="/"
+            className="flex items-center gap-2 text-lg font-semibold md:text-base"
+          >
+            <School className="h-6 w-6" />
+            <span className="sr-only">مساعد المشرف</span>
+          </Link>
+          <AppLink to="/" link="الرئيسية" />
+          <AppLink to="/allStudents" link="كل التلاميذ" />
+          <AppLink to="/absences" link="الغيابات" />
+          <AppLink to="/mark-absences" link="تسجيل الغيابات" />
+          {/* <AppLink to="/absences" link="اشعارات" /> */}
+          <NavigationMenu className="text-right">
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <NavigationMenuTrigger className="text-muted-foreground transition-colors hover:text-foreground">
+                  قوائم
+                </NavigationMenuTrigger>
+                <NavigationMenuContent>
+                  <ul className="grid gap-1 p-4">
+                    <AppLink to="/maafiyin" link="المعفيين" subLink />
+                    <AppLink to="/moghadirin" link="المغادرين" subLink />
+                    <AppLink to="/wafidin" link="الوافدين" subLink />
+                    <AppLink to="/otlaMaradiya" link="العطل المرضية" subLink />
+                    <AppLink to="/mo3idin" link="المعيدين" subLink />
+                    <AppLink to="/mamnouhin" link="الممنوحين" subLink />
+                    <AppLink to="/mosadidin" link="المسددين" subLink />
+                  </ul>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+          <AppLink to="/print-pdf" link="طباعة" />
+          <AppLink to="/student-cards" link="بطاقات" />
+          <AppLink to="/add-student" link="إضافة تلميذ" />
+          {/* <AppLink to="/daily-report" link="التقرير اليومي" /> */}
+        </nav>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button
+              variant="outline"
+              size="icon"
+              className="shrink-0 md:hidden"
             >
-              <School className="h-6 w-6" />
-              <span className="sr-only">Acme Inc</span>
-            </Link>
-            <AppLink to="/" link="الرئيسية" />
-            <AppLink to="/allStudents" link="كل التلاميذ" />
-            <AppLink to="/absences" link="الغيابات" />
-            <AppLink to="/mark-absences" link="تسجيل الغيابات" />
-            {/* <AppLink to="/Absence" link="اشعارات" /> */}
-            <AppLink to="/print-pdf" link="طباعة" />
-            {/* <AppLink to="/daily-report" link="التقرير اليومي" /> */}
-          </nav>
-        </SheetContent>
-      </Sheet>
-      <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4 text-right">
-        <form className="ml-auto flex-1 sm:flex-initial">
+              <Menu className="h-5 w-5" />
+              <span className="sr-only">Toggle navigation menu</span>
+            </Button>
+          </SheetTrigger>
+          <SheetContent side="right" id="sheet">
+            <nav dir="rtl" className="grid gap-6 text-lg font-medium">
+              <Link
+                to="/"
+                className="flex items-center gap-2 text-lg font-semibold"
+              >
+                <School className="h-6 w-6" />
+                <span className="sr-only">Acme Inc</span>
+              </Link>
+              <AppLink to="/" link="الرئيسية" />
+              <AppLink to="/allStudents" link="كل التلاميذ" />
+              <AppLink to="/absences" link="الغيابات" />
+              <AppLink to="/mark-absences" link="تسجيل الغيابات" />
+              {/* <AppLink to="/Absence" link="اشعارات" /> */}
+              <AppLink to="/print-pdf" link="طباعة" />
+              {/* <AppLink to="/daily-report" link="التقرير اليومي" /> */}
+            </nav>
+          </SheetContent>
+        </Sheet>
+      </div>
+      <div className="flex items-center justify-end w-full gap-4 md:ml-auto md:gap-2 lg:gap-4 text-right">
+        {/* <form className="ml-auto flex-1 sm:flex-initial">
           <div className="relative">
             <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
             <Input
@@ -114,7 +120,7 @@ function HeaderNavbar() {
               className="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]"
             />
           </div>
-        </form>
+        </form> */}
         <ModeToggle />
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
